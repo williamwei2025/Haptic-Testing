8f23320d5432fbaaf0fcd474fc0b0aa4f11311c9
William Wei <weiwilli@usc.edu>
2022-12-07 02:27:59 -0800

    Initial Commit

---
+++ HapticsTest/HapticsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsTestApp.swift
+//  HapticsTest
+//
+//  Created by William Wei on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct HapticsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
